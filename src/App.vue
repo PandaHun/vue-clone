71c87307ba4c06cdee277f357f0033e5fdaf311c
PandaHun <gnsgnsek@gmail.com>
2022-01-27 18:05:55 +0900

    feat/Billboard: Create Billboard

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <LNB />
   <RNB />
   <TheHeader />
+  <Billboard />
 </template>
 
 <script>
 import LNB from '~/components/LeftNavigationBar'
 import RNB from '~/components/RightNavigationBar'
 import TheHeader from '~/components/TheHeader'
+import Billboard from '~/components/Billboard'
 
 export default {
   components: {
     LNB,
     RNB,
-    TheHeader
+    TheHeader,
+    Billboard
   }
 }
 </script>
