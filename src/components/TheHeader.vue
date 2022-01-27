2d5914fe2e8f174f35460160169fa69541e53795
PandaHun <gnsgnsek@gmail.com>
2022-01-27 15:46:45 +0900

    feat/Header: Create Header rankingList

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,15 +1,15 @@
 <template>
-  <header>
+  <header :class="{ fixed: isFixed }">
     <div class="inner">
       <div
         class="open-nav-drawer"
-        @click="onNav">
-      </div>
+        @click="onNav('LNB')"></div>
       <a
         href="/"
         class="logo"></a>
       <div class="search">
         <input
+          v-model="searchText"
           type="text"
           placeholder="찾고 싶은 상품을 검색해 보세요!"
           @keypress.enter="search" />
@@ -36,22 +36,79 @@
         <div
           class="open-more"
           @click="toggleRankingWrap"></div>
-      </div>
-      <div class="user-menu">
+        <div
+          v-if="isShowRankingWrap"
+          class="ranking-wrap"
+          @click.stop="">
+          <div class="title">
+            <h3>실시간 쇼핑 검색어</h3>
+            <div class="time">
+              {{ referenceDate }} 기준
+            </div>
+            <div
+              class="close-wrap"
+              @click="toggleRankingWrap"></div>
+          </div>
+          <div class="tabs">
+            <div
+              :class="{ active: !tabIndex }"
+              class="tab"
+              @click="tabIndex = 0">
+              1 ~ 10위
+            </div>
+            <div
+              :class="{ active: tabIndex }"
+              class="tab"
+              @click="tabIndex = 1">
+              11 ~ 20위
+            </div>
+          </div>
+          <ul class="list">
+            <li
+              v-for="(rank, index) in filterRankings"
+              :key="rank.name">
+              <a :href="rank.href">
+                <span class="index">{{ (tabIndex * 10 ) + index + 1 }}</span>
+                <span class="name">{{ rank.name }}</span>
+                <span class="relative-name"> {{ rank.relativeName }}</span>
+              </a>
+              <div
+                :class="rank.status"
+                class="icon"></div>
+            </li>
+          </ul>
+        </div>
+        <div class="user-menu">
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
-import Swiper from 'swiper'
-import 'swiper/css'
+import dayjs from 'dayjs'
+import Swiper from 'swiper/bundle'
+import 'swiper/css/bundle'
 
 export default {
   data() {
     return {
+      searchText: '',
       rankings: {},
-      searchText: ''
+      isShowRankingWrap: false,
+      tabIndex: 0
+    }
+  },
+  computed: {
+    referenceDate () {
+      return dayjs(this.rankings.referenceDate).format('YYYY.MM.DD HH:mm')
+    },
+    filterRankings () {
+      return this.rankings.rankings.filter((rank, index)=> {
+        const start = this.tabIndex * 10
+        const end = start + 9
+        return start <= index && index <= end
+      })
     }
   },
   mounted() {
@@ -232,6 +289,124 @@ header {
         transform: rotate(45deg);
       }
     }
+          .ranking-wrap {
+        position: absolute;
+        top: 44px;
+        z-index: 2;
+        width: 410px;
+        padding: 18px 25px 25px;
+        border: 1px solid #eee;
+        border-radius: 4px;
+        box-sizing: border-box;
+        box-shadow: 0 6px 24px rgba(#000,.1);
+        background-color: #fff;
+        .title {
+          display: flex;
+          align-items: flex-end;
+          h3 {
+            font-size: 17px;
+            font-weight: 700;
+          }
+          .time {
+            margin-left: 10px;
+            font-size: 13px;
+            color: #999;
+            flex: 1;
+          }
+          .close-wrap {
+            width: 36px;
+            height: 36px;
+            background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+            background-position: -261px -203px;
+            background-size: 363px;
+            cursor: pointer;
+            margin: -6px -10px;
+            transform: scale(.6666); // from 36px to 24px
+          }
+        }
+        .tabs {
+          height: 40px;
+          margin: 18px 0;
+          border: 1px solid #eee;
+          box-sizing: border-box;
+          background-color: #fafafa;
+          display: flex;
+          .tab {
+            flex: 1;
+            color: #7d7d7d;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+            &:first-child {
+              border-right: 1px solid #eee;
+            }
+            &.active {
+              background-color: #fff;
+              font-weight: 700;
+              color: #333;
+            }
+          }
+        }
+        .list {
+          li {
+            position: relative;
+            margin-top: 14px;
+            &:first-child {
+              margin-top: 0;
+            }
+            a {
+              display: block;
+              height: 22px;
+              line-height: 22px;
+              text-decoration: none;
+              font-size: 15px;
+              color: #000;
+              .index {
+                font-weight: 700;
+                margin-right: 10px;
+                color: #f43142;
+                font-style: italic;
+              }
+              .relative-name {
+                margin-left: 4px;
+                font-size: 13px;
+                color: #999;
+              }
+            }
+            .icon {
+              position: absolute;
+              top: 0;
+              right: 0;
+              width: 22px;
+              height: 22px;
+              background-image: url("https://trusting-williams-8cacfb.netlify.app/images/globals_2x.png");
+              background-size: 363px;
+              &.stay {
+                background-image: none;
+                &::after {
+                  content: "";
+                  position: absolute;
+                  top: 0;
+                  bottom: 0;
+                  left: 0;
+                  right: 0;
+                  margin: auto;
+                  width: 6px;
+                  height: 2px;
+                  background: #999;
+                }
+              }
+              &.up {
+                background-position: -110px -251px;
+              }
+              &.down {
+                background-position: -58px -251px;
+              }
+            }
+          }
+        }
+      }
   }
 }
 </style>
