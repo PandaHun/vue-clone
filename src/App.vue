4c97ec54f4192535f4d16ea34f9cab1936a2406b
PandaHun <gnsgnsek@gmail.com>
2022-01-27 17:42:07 +0900

    feat/RNB: Create RightNavigationBar recent-views

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <LNB />
+  <RNB />
   <TheHeader />
 </template>
 
 <script>
 import LNB from '~/components/LeftNavigationBar'
+import RNB from '~/components/RightNavigationBar'
 import TheHeader from '~/components/TheHeader'
 
 export default {
   components: {
     LNB,
+    RNB,
     TheHeader
   }
 }
